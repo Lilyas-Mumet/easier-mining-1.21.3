4cd0e46c37f67e9804126796acf78c44a191418f
Lilyas <lilyas.artatila@gmail.com>
2024-11-04 18:23:53 +0700

    Initial commit

---
+++ src/main/java/net/lilyas/easiermining/EasierMining.java
@@ -0,0 +1,18 @@
+package net.lilyas.easiermining;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class EasierMining implements ModInitializer {
+	public static final String MOD_ID = "easier-mining";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+		LOGGER.info("Hello Fabric world!");
+	}
+}
---
+++ src/main/java/net/lilyas/easiermining/EasierMiningClient.java
@@ -0,0 +1,10 @@
+package net.lilyas.easiermining;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EasierMiningClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
